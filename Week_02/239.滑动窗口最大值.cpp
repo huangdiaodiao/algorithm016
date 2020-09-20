a04833891bdef427050c544b3ba520c26514eeff
huangdiaodiao <huangdiaopeng@163.com>
2020-09-20 20:51:46 +0800

    Modified   Week_02/239.滑动窗口最大值.cpp

--- Week_02/239.滑动窗口最大值.cpp
+++ Week_02/239.滑动窗口最大值.cpp
@@ -15,3 +15,23 @@ public:
         return ans;
     }
 };
+
+// 用堆来完成
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        priority_queue<pair<int, int>> que;
+        for (int j = 0; j < nums.size(); ++j) {
+            que.push({nums[j], j});
+            if (j >= k - 1) {
+                // (j-k+1)是窗口第一个元素的左边, 如果队列中的最大元素超过了这个范围, 那么就要将最大值弹出去
+                while (que.top().second < j - k + 1)
+                    que.pop();
+                ans.push_back(que.top().first);
+            }
+        }
+        return ans;
+    }
+};
+
